a5501ed36ef677c2b01777f611c4a1037288545a
Andreas <awiesing90@gmail.com>
2017-02-12 14:25:45 +0100

    Removed ellipse from help text and fixed version number to 1.0

--- src/main/java/com/electrocnic/blocklines/Commands/Modes/ChangeMode.java
+++ src/main/java/com/electrocnic/blocklines/Commands/Modes/ChangeMode.java
@@ -27,7 +27,7 @@ public class ChangeMode implements ICommand {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
         if(args.length <= 1) {
-            sender.sendMessage(new TextComponentString("Mode needs one of these arguments: line, ellipse, circle, cube."));
+            sender.sendMessage(new TextComponentString("Mode needs one of these arguments: line, circle, cube."));
             return;
         }
         ICommand command = subcommands.get(args[1]);
@@ -40,17 +40,17 @@ public class ChangeMode implements ICommand {
 
     @Override
     public String toString() {
-        return "/bl mode <ModeType> [0|1|2|3|t|m|a|f]\n- Available ModeTypes: circle, ellipse, line, cube\n" +
+        return "/bl mode <ModeType> [0|1|2|3|t|m|a|f]\n- Available ModeTypes: circle, line, cube\n" +
                 //"/bl mode " + subcommands.entrySet().stream().map(Map.Entry::toString).collect(Collectors.joining(System.lineSeparator()+"/bl mode ")) +
                 //"\n- /bl mode <circle|ellipse> [0|1|2|3|t|m|a|f] -\n" +
-                BlockLinesCommands.addSpaces("") + "0: A circle/ellipse will be drawn.\n" +
+                BlockLinesCommands.addSpaces("") + "0: A circle will be drawn.\n" +
                 BlockLinesCommands.addSpaces("") + "1: Only the part between the selected blocks will be drawn.\n" +
                 BlockLinesCommands.addSpaces("") + "2: Only the part outside the selected blocks will be drawn.\n" +
                 BlockLinesCommands.addSpaces("") + "3: Only the first segment between selection 1 and selection 2 will be drawn.\n" +
                 BlockLinesCommands.addSpaces("") + "a: Turns the placement of blocks to overwrite non-air blocks on or off.\n" +
                 BlockLinesCommands.addSpaces("") + "m: Turns the placement of the middle of the circle on or off.\n" +
                 BlockLinesCommands.addSpaces("") + "t: The circle will be drawn with thick lines.\n" +
-                BlockLinesCommands.addSpaces("") + "f: A filled circle/ellipse will be drawn.\n" +
+                BlockLinesCommands.addSpaces("") + "f: A filled circle will be drawn.\n" +
                 "- /bl mode line [row|next] - For drawing more than one lines at the same time.\n" +
                 BlockLinesCommands.addSpaces("") + "row: starts the \"In a row\" Mode: Several lines can be drawn with low effort.\n" +
                 BlockLinesCommands.addSpaces("") + "next: starts the second selection sequence, if \"In a row\" is active.\n" +
